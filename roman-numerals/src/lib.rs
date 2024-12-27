47a293b6dcd85367aaedd1d5c32423cb2a6e8668
annieversary <annie@versary.town>
2024-12-27 12:37:44 +0100

    basic implementation of roman numerals

--- roman-numerals/src/lib.rs
+++ roman-numerals/src/lib.rs
@@ -1,4 +1,4 @@
-use harfbuzz_wasm::{debug, Font, Glyph, GlyphBuffer};
+use harfbuzz_wasm::{Font, Glyph, GlyphBuffer};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -11,47 +11,107 @@ pub fn shape(
 ) -> i32 {
     let font = Font::from_ref(font_ref);
     let mut buffer = GlyphBuffer::from_ref(buf_ref);
-    // Get buffer as string
-    let buf_u8: Vec<u8> = buffer.glyphs.iter().map(|g| g.codepoint as u8).collect();
-    let str_buf = String::from_utf8_lossy(&buf_u8);
 
-    // let re = Regex::new(r"(\d+(?:\.\d+)?)\s*([\+-/\*])\s*(\d+(?:\.\d+)?)\s*=").unwrap();
-    // if let Some(captures) = re.captures(&str_buf) {
-    //     debug(&format!("{:?}", captures));
-    //     let left = captures[1].parse::<f32>().unwrap();
-    //     let right = captures[3].parse::<f32>().unwrap();
-    //     let res = match &captures[2] {
-    //         "+" => left + right,
-    //         "-" => left - right,
-    //         "*" => left * right,
-    //         "/" => left / right,
-    //         _ => panic!(),
-    //     };
-    //     let res_str = format!("{}", res);
-    //     buffer.glyphs = res_str
-    //         .chars()
-    //         .enumerate()
-    //         .map(|(ix, x)| Glyph {
-    //             codepoint: x as u32,
-    //             flags: 0,
-    //             x_advance: 0,
-    //             y_advance: 0,
-    //             cluster: ix as u32,
-    //             x_offset: 0,
-    //             y_offset: 0,
-    //         })
-    //         .collect();
-    // } else {
-    //     debug("No match");
-    //     debug(&str_buf);
-    // }
+    let mut out = vec![];
 
-    for mut item in buffer.glyphs.iter_mut() {
+    let mut digits = vec![];
+    for item in buffer.glyphs.iter() {
+        if let Some(number) = unicode_codepoint_to_number(item.codepoint) {
+            digits.push(number);
+        } else {
+            process_digits(&mut digits, &mut out);
+
+            out.push(*item);
+        }
+    }
+
+    // also handle non empty digits here, otherwise numbers at the end of the string won't work
+    process_digits(&mut digits, &mut out);
+
+    // fix characters
+    for item in out.iter_mut() {
         // Map character to glyph
         item.codepoint = font.get_glyph(item.codepoint, 0);
         // Set advance width
         item.x_advance = font.get_glyph_h_advance(item.codepoint);
     }
+
+    buffer.glyphs = out;
+
     // Buffer is written back to HB on drop
     1
+}
+
+fn unicode_codepoint_to_number(unicode: u32) -> Option<u8> {
+    if (0x30..=0x39).contains(&unicode) {
+        Some((unicode - 0x30) as u8)
+    } else {
+        None
+    }
+}
+
+fn process_digits(digits: &mut Vec<u8>, out: &mut Vec<Glyph>) {
+    // if we had some numbers, and now we're in a non-number now
+    // it means we gotta render the numbers
+    if !digits.is_empty() {
+        // turn digits into a single number...
+        let number = digits_to_number(digits);
+        // ...then turn number to roman numerals string...
+        let roman = number_to_roman_numeral(number);
+        let roman_glyphs = string_to_glyphs(&roman);
+        // ...and add the roman glyphs to the output
+        out.extend_from_slice(&roman_glyphs);
+
+        digits.clear();
+    }
+}
+
+fn digits_to_number(digits: &[u8]) -> u64 {
+    digits.iter().rev().enumerate().fold(0, |acc, (idx, num)| {
+        acc + (*num as u64) * 10u64.pow(idx as u32)
+    })
+}
+
+fn string_to_glyphs(string: &str) -> Vec<Glyph> {
+    string
+        .chars()
+        .enumerate()
+        .map(|(ix, x)| Glyph {
+            codepoint: x as u32,
+            flags: 0,
+            x_advance: 0,
+            y_advance: 0,
+            cluster: ix as u32,
+            x_offset: 0,
+            y_offset: 0,
+        })
+        .collect()
+}
+
+fn number_to_roman_numeral(mut number: u64) -> String {
+    let letters = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I"),
+    ];
+    let mut result = String::new();
+
+    for (value, symbol) in letters {
+        while number >= value {
+            result.push_str(symbol);
+            number -= value;
+        }
+    }
+
+    result
 }
